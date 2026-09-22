67131fe9da765e2fb5a9955661c45f0d6655c96d
Miko Gronroos <miko.gronroos@gmail.com>
2026-09-22 22:41:32 +0300

    Add getting commandregistry

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 int main(){
   printf("Welcome to mitten\n");
   printf("Type 'help' for list of all available commands\n");
-  initialize();
+  registry* = initialize();
   char input[256];
   while(true){
     if(scanf("%255s", input) == 1){
