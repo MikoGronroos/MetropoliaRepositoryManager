bc3f1277766267609d682cc0ef3ae93ab2dccf84
Miko Gronroos <miko.gronroos@gmail.com>
2026-09-23 02:50:20 +0300

    Fix semicolon from about text

--- src/cabout.c
+++ src/cabout.c
@@ -4,7 +4,7 @@
 int about(){
 
   printf("Mitten is a tool for managing repositories\n");
-  printf("Project for metropolia c course in Smart Iot Embedded\n")
+  printf("Project for metropolia c course in Smart Iot Embedded\n");
   printf("Made by Miko Grönroos\n");
 
 }
