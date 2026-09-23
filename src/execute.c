69632106aa626b8d49b816ff1b2020d05f41e190
Miko Gronroos <miko.gronroos@gmail.com>
2026-09-23 03:36:43 +0300

    Fix command array size limit

--- src/execute.c
+++ src/execute.c
@@ -2,16 +2,18 @@
 #include <string.h>
 #include "commands.h"
 
+#define amountOfCommands 3
+
 int execute(char* input){
   
-  command cmds[2] = {
+  command cmds[amountOfCommands] = {
     {"help", help},
     {"about", about},
     {"add", add}
   };
 
   int error = 1; 
-  for(int i = 0; i < 2; i++){
+  for(int i = 0; i < amountOfCommands; i++){
     if(strcmp(input, cmds[i].commandName) == 0){
       cmds[i].ptr();
       error = 0;
